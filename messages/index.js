f1ce49592f505e9ab40c4990d56da54970b467e7
Ray Kao <ray@peopleandcode.com>
2018-01-23 11:19:42 -0700

    removed storage settings - was causing the app to fail silently.

--- messages/index.js
+++ messages/index.js
@@ -6,13 +6,10 @@ var botbuilder_azure = require("botbuilder-azure");
 var builder_cognitiveservices = require("botbuilder-cognitiveservices");
 var path = require('path');
 
-var useEmulator = (process.env.NODE_ENV == 'development');
+var useEmulator = true;
 
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
-});
+var connector = new builder.ChatConnector();
+
 
 /*----------------------------------------------------------------------------------------
 * Bot Storage: This is a great spot to register the private state storage for your bot. 
@@ -25,42 +22,29 @@ var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 var bot = new builder.UniversalBot(connector);
-bot.localePath(path.join(__dirname, './locale'));
-bot.set('storage', tableStorage);
-
-var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
-                knowledgeBaseId: process.env.QnAKnowledgebaseId, 
-    subscriptionKey: process.env.QnASubscriptionKey});
-
-var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
-    recognizers: [recognizer],
-                defaultMessage: 'No match! Try changing the query terms!',
-                qnaThreshold: 0.3}
-);
-
-bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
-
-bot.dialog('/', //basicQnAMakerDialog);
-[
-    function (session){
-        var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
-        var qnaSubscriptionKey = process.env.QnASubscriptionKey;
-        
-        // QnA Subscription Key and KnowledgeBase Id null verification
-        if((qnaSubscriptionKey == null || qnaSubscriptionKey == '') || (qnaKnowledgebaseId == null || qnaKnowledgebaseId == ''))
-            session.send('Please set QnAKnowledgebaseId and QnASubscriptionKey in App Settings. Get them at https://qnamaker.ai.');
-        else
-            session.replaceDialog('basicQnAMakerDialog');
-    }
-]);
 
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
+    server.post('/api/messages', connector.listen());    
+
     server.listen(3978, function() {
+        console.log("using emulator");
         console.log('test bot endpont at http://localhost:3978/api/messages');
     });
-    server.post('/api/messages', connector.listen());    
 } else {
     module.exports = connector.listen();
 }
+
+bot.dialog('/', function (session){
+    var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
+    var qnaSubscriptionKey = process.env.QnASubscriptionKey;
+    
+    // QnA Subscription Key and KnowledgeBase Id null verification
+    if((qnaSubscriptionKey == null || qnaSubscriptionKey == '') || (qnaKnowledgebaseId == null || qnaKnowledgebaseId == ''))
+        session.send('Please set QnAKnowledgebaseId and QnASubscriptionKey in App Settings. Get them at https://qnamaker.ai.');
+    else
+        session.replaceDialog('basicQnAMakerDialog');
+});
+
+
